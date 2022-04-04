a48eb634ed115879848d30e0703aff7efeea0e35
Aurélien Manteaux <amanteaux@coreoz.com>
2022-04-04 15:06:23 +0200

    Add comments

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -1,3 +1,6 @@
+// node-fetch@2 is required since jest does not support node-fetch 3
+// eslint-disable-next-line max-len
+// cf https://stackoverflow.com/questions/69383514/node-fetch-3-0-0-and-jest-gives-syntaxerror-cannot-use-import-statement-outside
 const nodeFetch = require('node-fetch');
 const { Headers, Request, Response } = require('node-fetch');
 
@@ -6,4 +9,5 @@ global.Headers = Headers;
 global.Request = Request;
 global.Response = Response;
 
+// so TS does not raise errors because there is no import statement
 export {};
