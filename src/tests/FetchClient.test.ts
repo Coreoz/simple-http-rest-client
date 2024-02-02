d868d941ca9fb4db8011583d161930792dbc969c
Vincent Dubois <vdubois@coreoz.com>
2024-02-02 18:10:08 +0100

    Add rest request tests

--- src/tests/FetchClient.test.ts
+++ src/tests/FetchClient.test.ts
@@ -1,8 +1,8 @@
-import { HttpRequest, HttpMethod } from 'simple-http-request-builder';
-import ApiHttpClient from './ApiHttpClient';
-import { genericError, networkError, timeoutError } from '../lib/client/HttpResponse';
+import { HttpMethod, HttpRequest } from 'simple-http-request-builder';
 import { fetchClient } from '../lib/client/FetchClient';
+import { genericError, networkError, timeoutError } from '../lib/client/HttpResponse';
 import { unwrapHttpPromise } from '../lib/promise/HttpPromise';
+import ApiHttpClient from './ApiHttpClient';
 
 const waitTimeout = (durationInMillis: number) => new Promise(
   // eslint-disable-next-line no-promise-executor-return
@@ -14,6 +14,9 @@ const mockedFetchResponseStatus: ResponseInit = {
   status: 200,
   statusText: 'Ok',
   // headers
+  headers: {
+    'content-type': 'application/json',
+  },
 };
 let mockedPromiseError: Error | undefined;
 
@@ -27,6 +30,8 @@ const mockedFetch = (): Promise<Response> => {
   }
   return Promise.resolve(new Response(mockedFetchResponseBody, mockedFetchResponseStatus));
 };
+
+type MockUser = { id: number };
 
 describe('Tests fetch client', () => {
   const oldFetch = global.fetch;
@@ -77,7 +82,9 @@ describe('Tests fetch client', () => {
 
   test('Check handler execution error rejects promise with genericError', async () => {
     const httpClientWithErrorHandler = (httpRequest: HttpRequest<unknown>) => unwrapHttpPromise(fetchClient(
-      httpRequest, () => { throw new Error(); },
+      httpRequest, () => {
+        throw new Error();
+      },
     ));
     const response = new HttpRequest(
       httpClientWithErrorHandler,
@@ -86,5 +93,37 @@ describe('Tests fetch client', () => {
       '/users',
     ).execute();
     await expect(response).rejects.toEqual(genericError);
+  });
+
+  test('Check return type of rest request must be User', async () => {
+    // Mock body
+    const mockFirstUser: MockUser = { id: 1 };
+    setMockedBody(mockFirstUser);
+
+    // Add client
+    const apiClient: ApiHttpClient = new ApiHttpClient();
+    // Execute request in request
+    const result = await apiClient
+      .restRequest<MockUser>(HttpMethod.GET, 'https://hostname/users')
+      .execute();
+    expect(result.id).toEqual(1);
+  });
+
+  test('Check return type of then function must be unwrap in the next then call', async () => {
+    // Mock body
+    const mockFirstUser: MockUser = { id: 1 };
+    const mockSecondUser: MockUser = { id: 2 };
+    setMockedBody(mockFirstUser);
+
+    // Add client
+    const apiClient = new ApiHttpClient();
+    const request = apiClient.restRequest<MockUser>(HttpMethod.GET, 'https://hostname/users');
+    // Execute request in request
+    const result: MockUser = await request.execute()
+      .then(() => {
+        setMockedBody(mockSecondUser);
+        return request.execute();
+      });
+    expect(result.id).toEqual(2);
   });
 });
